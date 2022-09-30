909348baa8df551767567a4b06ddb84bdd8553cb
Sammantha73 <Samanthafeijoom@gmail.com>
2022-09-30 23:17:26 +0000

    mensaje para el commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+document.body.onload = function() {
+  myFunction();
 };
+
+function myFunction() {
+  let who = ["my dog", "my granma", "my turtle", "my bird"];
+  let what = ["eated", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished my job",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  num = who.length;
+  num2 = what.length;
+  num3 = when.length;
+
+  var ramdomwho = Math.floor(Math.random() * num);
+  var randomwhat = Math.floor(Math.random() * num2);
+  var randomwhen = Math.floor(Math.random() * num3);
+
+  document.getElementById("excuse").innerHTML =
+    "You Will no belive <br>" +
+    who[ramdomwho] +
+    " " +
+    what[randomwhat] +
+    " " +
+    when[randomwhen];
+}
